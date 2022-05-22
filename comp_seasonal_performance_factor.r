35a2624f40e3df4883b44da49884f1a750da2773
Ayoub EL HOUDRI <71386458+y-aoub@users.noreply.github.com>
2022-05-22 18:23:33 +0200

    Update comp_seasonal_performance_factor.r

--- comp_seasonal_performance_factor.r
+++ comp_seasonal_performance_factor.r
@@ -1,7 +1,7 @@
 serie = read.table("data/beer_projet_app.csv", header = F, sep=",")
-serie = serie[,2] # On garde seulement la colonne des valeurs 
+serie = serie[,2] # We keep just the values column
 serie = ts(data = serie, start = c(1974,1), frequency = 12) # Transformation en série temporelle
-# Coefficients saisonniers
+# Seasonal factors
 CS = decompose(serie)$figure
 
 
